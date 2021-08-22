834618a356ecad52d94a7aa8b6fb1d98db77a3d0
dpervush <dpervush>
2021-08-22 21:34:47 +0300

    update cors, fix delete method

--- server/Card/CardController.js
+++ server/Card/CardController.js
@@ -1,48 +1,47 @@
 import CardService from "./CardService.js";
 
 class CardController {
-    async create(req, res) {
-        try {
-            const card = await CardService.create(req.body)
-            res.json(card)
-        } catch (e) {
-            res.status(500).json(e)
-        }
+  async create(req, res) {
+    try {
+      const card = await CardService.create(req.body);
+      res.json(card);
+    } catch (e) {
+      res.status(500).json(e);
     }
+  }
 
-    async getAll(req, res) {
-        try {
-            const cards = await CardService.getAll();
-            return res.json(cards);
-        } catch (e) {
-            res.status(500).json(e)
-        }
+  async getAll(req, res) {
+    try {
+      const cards = await CardService.getAll();
+      return res.json(cards);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async getOne(req, res) {
-        try {
-            const card = await CardService.getOne(req.params.id)
-            return res.json(card)
-        } catch (e) {
-            res.status(500).json(e)
-        }
+  }
+  async getOne(req, res) {
+    try {
+      const card = await CardService.getOne(req.params.id);
+      return res.json(card);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async update(req, res) {
-        try {
-            const updatedCard = await CardService.update(req.body);
-            return res.json(updatedCard);
-        } catch (e) {
-            res.status(500).json(e.message)
-        }
+  }
+  async update(req, res) {
+    try {
+      const updatedCard = await CardService.update(req.body);
+      return res.json(updatedCard);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-    async delete(req, res) {
-        try {
-            const card = await CardService.create(req.params.id);
-            return res.json(card)
-        } catch (e) {
-            res.status(500).json(e)
-        }
+  }
+  async delete(req, res) {
+    try {
+      const card = await CardService.delete(req.params.id);
+      return res.json(card);
+    } catch (e) {
+      res.status(500).json(e);
     }
+  }
 }
-
 
 export default new CardController();
