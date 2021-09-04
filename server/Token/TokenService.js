cf385e8244c2a131cc950b73b703ba2a48fbb331
dpervush <dpervush>
2021-09-04 12:26:36 +0300

    generate tokens

---
+++ server/Token/TokenService.js
@@ -0,0 +1,40 @@
+import jsonwebtoken from "jsonwebtoken";
+import Token from "./Token.js";
+
+class TokenService {
+  async generateToken(payload) {
+    const accessToken = jsonwebtoken.sign(
+      payload,
+      process.env.JWT_ACCESS_SECRET,
+      {
+        expiresIn: "30m",
+      }
+    );
+    const refreshToken = jsonwebtoken.sign(
+      payload,
+      process.env.JWT_REFRESH_SECRET,
+      {
+        expiresIn: "10d",
+      }
+    );
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await Token.findOne({ user: userId });
+
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+
+    const token = await Token.create({ user: userId, refreshToken });
+    return token;
+  }
+}
+
+export default new TokenService();
