188de384f781a09232609c86e7565354db85a4d1
dpervush <dpervush>
2021-11-22 22:02:48 +0300

    display statistics for periods

--- client/components/ShortStat/ShortStat.jsx
+++ client/components/ShortStat/ShortStat.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getStatsByPeriod } from "../../store/slices/stats";
 import Dropdown from "../UI/Dropdown/Dropdown";
 import PostItem from "./PostItem/PostItem";
 
@@ -28,15 +30,69 @@ const state = {
 };
 
 const ShortStat = () => {
+  const dispatch = useDispatch();
+
+  const statsByPeriod = useSelector(({ stats }) => stats.statsByPeriod);
+
+  const [selectedPeriod, setSelectedPeriod] = React.useState("week");
+
   const [dropdownState, setDropdownState] = React.useState(state);
+
   const resetThenSet = (id, items) => {
     const temp = [...items];
 
     temp.forEach((item) => (item.selected = false));
     temp[id].selected = true;
 
+    setSelectedPeriod(temp[id].title.toLowerCase());
+
     setDropdownState({ ...dropdownState, [items[0].key]: temp });
   };
+
+  React.useEffect(() => {
+    dispatch(getStatsByPeriod({ period: selectedPeriod }));
+  }, []);
+
+  React.useEffect(() => {
+    dispatch(getStatsByPeriod({ period: selectedPeriod }));
+  }, [dropdownState]);
+
+  const [statArray, setStatArray] = React.useState(null);
+
+  const fillStat = () => {
+    const statArray = Array(7).fill({});
+
+    for (let i = 0; i < statArray.length; i++) {
+      const weeks = Object.values(statsByPeriod);
+
+      if (selectedPeriod === "year") {
+        statArray[i] = { ...weeks[i] };
+      } else {
+        for (let key in weeks) {
+          statArray[i] = { ...Object.values(weeks[key])[i] };
+        }
+      }
+    }
+    return statArray;
+  };
+
+  const [maxs, setMaxs] = React.useState({ income: 0, expense: 0 });
+
+  React.useEffect(() => {
+    const statByPeriodArray = fillStat();
+    setStatArray([...statByPeriodArray]);
+
+    if (statArray) {
+      const maxIncome = Math.max(...statArray.map((item) => item.income ?? 0));
+      const maxExpense = Math.max(
+        ...statArray.map((item) => item.expense ?? 0)
+      );
+
+      setMaxs({ income: maxIncome, expense: maxExpense });
+    }
+  }, [statsByPeriod]);
+
+  console.log(statArray);
 
   return (
     <div className={styles.wrapper}>
@@ -49,13 +105,13 @@ const ShortStat = () => {
         />
       </div>
       <div className={styles.posts}>
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
-        <PostItem percentTop={70} percentBottom={20} />
+        {statArray?.map((item, index) => (
+          <PostItem
+            key={index}
+            percentTop={(item.expense / maxs.expense) * 50 || 0}
+            percentBottom={(item.income / maxs.income) * 30 || 0}
+          />
+        ))}
       </div>
     </div>
   );
