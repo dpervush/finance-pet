aa06858663eb46f7f1b89d6d7a8c9ca6a639076e
Daria Pervushina <daria.p@fora-soft.com>
2022-04-30 17:07:38 +0300

    fix get categories from first login

--- client/hooks/useCategories.js
+++ client/hooks/useCategories.js
@@ -1,22 +1,22 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { useLastMonthCategories } from "./useLastMonthCategories";
 
 export const useCategories = () => {
-  // const {
-  //   categories: categoriesBalanceExpense,
-  //   categoriesIncome: categoriesBalanceIncome
-  // } = useSelector(({ categories }) => categories);
+  const { categories, categoriesIncome } = useSelector(
+    ({ categories }) => categories
+  );
 
   const { statsByCategoryExpense, statsByCategoryIncome } = useSelector(
     ({ stats }) => stats
   );
 
   const { categoriesBalance: categoriesBalanceExpense } =
-    useLastMonthCategories(statsByCategoryExpense);
+    useLastMonthCategories(
+      statsByCategoryExpense.length > 0 ? statsByCategoryExpense : categories
+    );
 
   const { categoriesBalance: categoriesBalanceIncome } = useLastMonthCategories(
-    statsByCategoryIncome
+    statsByCategoryIncome.length > 0 ? statsByCategoryIncome : categoriesIncome
   );
 
   return [categoriesBalanceExpense, categoriesBalanceIncome];
