c9bd53f9bee28d6315a2d1a9a7a4c32265afd93e
dpervush <67655769+dpervush@users.noreply.github.com>
2022-05-01 22:09:46 +0300

    Update index.js

--- client/http/index.js
+++ client/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getWithExpiry } from "../utils/localStorageWithExpiry";
 
 // export const API_URL = "http://localhost:8080/api";
-export const API_URL = "http://www.891261-cd05421.tmweb.ru:8080/api";
+export const API_URL = "http://www.896903-cd05421.tmweb.ru:8080/api";
 
 const $api = axios.create({
   withCredentials: true,
