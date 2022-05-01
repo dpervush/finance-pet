baa95490e53379bd160d00583e73e3a15ec628b3
Daria Pervushina <daria.p@fora-soft.com>
2022-05-01 20:10:41 +0300

    add manifest

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -25,6 +25,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
